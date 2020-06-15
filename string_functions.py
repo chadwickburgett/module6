6e98f0c698c34c93ad3b1eb6369b62ff98934adb
Chadwick Burgett <54729703+chadwickburgett@users.noreply.github.com>
2020-06-15 15:53:10 -0500

    Module 6 Topic 3 assignment
    The main program takes user input of a string and int calls a function with those two variables as parameters.
    The function multiplies the string by the int and returns it for the main to print.

--- string_functions.py
+++ string_functions.py
@@ -3,8 +3,18 @@ Program: string_functions.py
 Author: Chadwick Burgett
 Last date modified: 06/15/2020
 
-
+This program takes user input of a string and int and
+passes it to a function that returns the string the
+number of times of the integer that the main then prints
 """
 def multiply_string(message,n):
-    """This     """
-    pass
+    """This function takes in a string and int and returns the string times the int, ie hi,2 = hihi
+    :param message: This is the string that will be returned
+    :param n: This is the int that will decide how many times the string is displayed
+    :return The string times the integer"""
+    return message * n
+
+if __name__ == '__main__':
+    phrase = input("Please enter a phrase: ")
+    num = int(input("Please enter a number between 1-9: "))
+    print(multiply_string(phrase, num)) #Function call and print command
--- test_string_functions.py
+++ test_string_functions.py
@@ -1,9 +1,20 @@
-import unittest
+"""
+Program: test_string_functions.py
+Author: Chadwick Burgett
+Last date modified: 06/15/2020
 
+This unittest tests the multiply_string function of the
+string_function program. It passes in a string and an int and checks
+if the function correctly multiplies them.
+"""
+import unittest
+from more_functions import string_functions as sf
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_multiple_string(self):
+        self.assertEqual("HIHIHI", sf.multiply_string("HI",3))
+        self.assertEqual("PythonPython", sf.multiply_string("Python",2))
+        self.assertEqual("ByeByeByeByeByeByeByeByeBye", sf.multiply_string("Bye",9))
 
 
 if __name__ == '__main__':
